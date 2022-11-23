b73545d4a7cb38ad79e2b1b592de2194340ba752
Genesis Thomas <22317248+genesisthomas@users.noreply.github.com>
2022-11-23 20:00:12 +0530

    init

--- app.js
+++ app.js
@@ -1,4 +1,48 @@
-const { bootstrap } = require("@kaholo/plugin-library");
+const { bootstrap, docker } = require("@kaholo/plugin-library");
+const path = require("path");
+
+const KAHOLO_POSTMAN_IMAGE = "postman/newman";
+
+async function runPostmanTest(params) {
+  const {
+    workingDirectory,
+    commands,
+    environmentalVariables = {},
+  } = params;
+
+  const absoluteWorkingDir = path.resolve(workingDirectory);
+  if (!await pathExists(absoluteWorkingDir)) {
+    throw new Error(`Path ${workingDirectory} does not exist on agent!`);
+  }
+
+  let commandToExecute;
+  // const postmanCommand = `node ${puppeteerJSFile}`;
+  commandToExecute = docker.buildDockerCommand({
+    image: KAHOLO_POSTMAN_IMAGE,
+    command: docker.sanitizeCommand(commands),
+    workingDirectory: "/etc/newman",
+    environmentVariables: environmentalVariables,
+    additionalArguments: [
+      "-v",
+      `${absoluteWorkingDir}:/etc/newman`,
+    ],
+  });
+
+  const commandOutput = await exec(commandToExecute, {
+    env: {
+      ...process.env,
+      ...environmentalVariables,
+    },
+  });
+
+  if (commandOutput.stderr && !commandOutput.stdout && !puppeteerCaptureCommand) {
+    throw new Error(commandOutput.stderr);
+  } else if (commandOutput.stderr) {
+    console.error(commandOutput.stderr);
+  }
+
+  return commandOutput.stdout;
+}
 
 async function hello(params) {
   const {
@@ -22,4 +66,5 @@ async function hello(params) {
 
 module.exports = bootstrap({
   hello,
+  runPostmanTest
 });
